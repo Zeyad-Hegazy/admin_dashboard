92e3e2be5682f55cb5b721e09f1c7187d6521774
Zeyad Hegazy <zhegazy409@gmail.com>
2023-09-24 18:53:19 +0300

    fix theme bug

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -14,10 +14,14 @@ export const ContextProvider = ({ children }) => {
 	const [isClicked, setIsClicked] = useState(initialState);
 	const [screenSize, setScreenSize] = useState(undefined);
 	const [currentColor, setCurrentColor] = useState(
-		localStorage.getItem("mainColor") || "#03C9D7"
+		localStorage.getItem("mainColor")
+			? localStorage.getItem("mainColor")
+			: "#03C9D7"
 	);
 	const [currentMode, setCurrentMode] = useState(
-		localStorage.getItem("themeMode") || "Light"
+		localStorage.getItem("themeMode")
+			? localStorage.getItem("themeMode")
+			: "Light"
 	);
 	const [themeSettings, setThemeSettings] = useState(false);
 
